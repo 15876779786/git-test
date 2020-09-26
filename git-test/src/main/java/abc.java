fe751f86ef83e484cdf947093aa3957987cb4050
chenxiongwei <15876779786@139.com>
2020-09-26 14:43:26 +0800

    修改abc文件，添加fun方法

--- git-test/src/main/java/abc.java
+++ git-test/src/main/java/abc.java
@@ -1,5 +1,9 @@
 public class abc {
     public static void main(String[] args) {
         System.out.println("abc");
+        //abc
+    }
+    public void fun(){
+        System.out.println("fun方法。。。");
     }
 }
